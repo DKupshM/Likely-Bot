0d0320eb4b17b615b5fea3aad7cd1cf829e90982
DKupshM <Dkupshm@gmail.com>
2016-06-29 22:59:20 -0700

    Init

---
+++ bot.js
@@ -0,0 +1,22 @@
+let util = require('util');
+let http = require('http');
+let random = require('rng');
+let Bot  = require('@kikinteractive/kik');
+
+// Configure the bot API endpoint, details for your bot
+let bot = new Bot({
+    username: 'LikelyBot',
+    apiKey: 'aa1f0716-387b-4649-b039-4acba079bbd4',
+    baseUrl: 'https://f469ea2c.ngrok.io'
+});
+
+bot.updateBotConfiguration();
+
+bot.onTextMessage((message) => {
+    message.reply('It is ' + (Math.floor(Math.random() * 100) + 1) + '% likely');
+});
+
+// Set up your server and start listening
+let server = http
+  .createServer(bot.incoming())
+  .listen(process.env.PORT || 8080);
